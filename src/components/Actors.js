a83cffbb569a468e8ddbce878887bb4670362489
Zac <zacharyflynn06@gmail.com>
2021-07-17 19:03:23 -0600

    commit tree d8c8d09c218218a0fcd6d2a304f22d3de0afbe3d

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  console.log(actors)
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((a, idx) => (
+        <div key={idx} className="actor">
+          <h3>Name: {a.name}</h3>
+          <h3>Movies:</h3>
+            <ul>
+              {a.movies.map((m, idx) => (
+                <li key={idx}>{m}</li>
+              ))}
+            </ul>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,20 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((m, idx) => (
+          <div key={idx}>
+            <h3>Title: {m.title}</h3>
+            <h4>Time: {m.time}</h4>
+            <h4>Genres:</h4>
+            <ul>
+              {m.genres.map((g, idx) => (
+                <li key={idx}>{g}</li>
+              ))}
+            </ul>
+          </div>
+        
+        ))}
     </div>
   );
 };
